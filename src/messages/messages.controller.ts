c0ae4513eef74e3e3230614be6d0a7728de77b07
Niranjan Kumar <niranjan.kushwaha@mediassist.in>
2022-11-27 20:07:36 +0530

    added dependency Injection

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,51 +1,24 @@
-// import { Controller, Get, Post, Body, Param } from '@nestjs/common';
-// import { CreateMessageDTO } from './dtos/create-message.dto';
-// import { MessagesService } from './messages.sevice';
-
-// @Controller('messages')
-// export class MessagesController {
-//   messagesService: MessagesService;
-//   constructor() {
-//     // we will remove and use dependency Injection
-//     this.messagesService = new MessagesService();
-//   }
-
-//   @Get()
-//   listMessages() {
-//     // although we are returning to server on get call but we will not use return keyword
-//     return this.messagesService.findAll();
-//     // return 'Hello';
-//   }
-
-//   @Post()
-//   createMessage(@Body() body: CreateMessageDTO) {
-//     console.log(body);
-//     // since our method is accepting only one param
-//     return this.messagesService.create(body.content);
-//   }
-
-//   //   if i will not pass id like this:@Param('id') and try to access param than it will give object of all params.
-//   @Get('/:id')
-//   getMessage(@Param('id') id: string) {
-//     console.log('id passed is: ', id);
-//     return this.messagesService.findOne(id);
-//   }
-// }
-
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDTO } from './dtos/create-message.dto';
 import { MessagesService } from './messages.sevice';
 
 @Controller('messages')
 export class MessagesController {
-  messagesService: MessagesService;
+  // ================> old way to get instance of a class without dependency Injection
+  // messagesService: MessagesService;
+  // constructor() {
+  //   this.messagesService = new MessagesService();
+  // }
 
-  constructor() {
-    // DONT DO THIS ON REAL APP
-    // USE DEPENDENCY INJECTION
-    this.messagesService = new MessagesService();
-  }
-
+  // =================> with depedency Injection
+  constructor(public messagesService: MessagesService) {}
   @Get()
   listMessages() {
     return this.messagesService.findAll();
@@ -58,7 +31,11 @@ export class MessagesController {
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    return this.messagesService.findOne(id);
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+    return message;
   }
 }
--- src/messages/messages.sevice.ts
+++ src/messages/messages.sevice.ts
@@ -1,33 +1,17 @@
-// import { MessagesRepository } from './messaes.repository';
-
-// export class MessagesService {
-//   messagesRepo: MessagesRepository;
-//   constructor() {
-//     this.messagesRepo = new MessagesRepository();
-//   }
-
-//   findOne(id: string) {
-//     return this.messagesRepo.findOne(id);
-//   }
-//   findAll() {
-//     return this.messagesRepo.findAll();
-//   }
-
-//   create(content: string) {
-//     return this.messagesRepo.create(content);
-//   }
-// }
-
+import { Injectable } from '@nestjs/common';
 import { MessagesRepository } from './messaes.repository';
 
+@Injectable()
 export class MessagesService {
-  messagesRepo: MessagesRepository;
+  //   messagesRepo: MessagesRepository;
 
-  constructor() {
-    // Service is creating its own dependencies
-    // DONT DO THIS ON REAL APPS
-    this.messagesRepo = new MessagesRepository();
-  }
+  //   constructor() {
+  //     // Service is creating its own dependencies
+  //     // DONT DO THIS ON REAL APPS
+  //     this.messagesRepo = new MessagesRepository();
+  //   }
+
+  constructor(public messagesRepo: MessagesRepository) {}
 
   findOne(id: string) {
     return this.messagesRepo.findOne(id);
